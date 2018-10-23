3d94b1e81bee67975452c2e09664891ed6cc8315
Chris Mears <chris.mears@gmail.com>
2018-10-23 13:06:52 -0700

    Update User model to only store what is needed
    - and Prettier'd

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,77 +1,71 @@
-const passport = require('passport')
-const Oauth2Strategy = require('passport-oauth2')
-const request = require('request')
-const User = require('../models/user-model')
+const passport = require('passport');
+const Oauth2Strategy = require('passport-oauth2');
+const request = require('request');
+const User = require('../models/user-model');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user)
-  })
-})
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
-passport.use(new Oauth2Strategy({
-  authorizationURL: 'https://todoist.com/oauth/authorize',
-  tokenURL: 'https://todoist.com/oauth/access_token',
-  clientID: process.env.PASSPORT_CLIENT_ID,
-  clientSecret: process.env.PASSPORT_CLIENT_SECRET,
-  callbackURL: '/auth/todoist/redirect',
-  scope: 'data:read'
-}, (accessToken, refreshToken, profile, done) => {
-  // NB: A Todoist profile isn't returned, so we need to request one
-  request(
+passport.use(
+  new Oauth2Strategy(
     {
-      url: 'https://todoist.com/api/v7/sync',
-      qs: { token: accessToken, sync_token: '*', resource_types: '["user", "projects"]' }
+      authorizationURL: 'https://todoist.com/oauth/authorize',
+      tokenURL: 'https://todoist.com/oauth/access_token',
+      clientID: process.env.PASSPORT_CLIENT_ID,
+      clientSecret: process.env.PASSPORT_CLIENT_SECRET,
+      callbackURL: '/auth/todoist/redirect',
+      scope: 'data:read',
     },
-    function (error, response, body) {
-      const todoistSyncData = JSON.parse(body)
-      const todoistUser = todoistSyncData.user
-      const todoistProjects = todoistSyncData.projects
+    (accessToken, refreshToken, profile, done) => {
+      // NB: A Todoist profile isn't returned, so we need to request one
+      request(
+        {
+          url: 'https://todoist.com/api/v7/sync',
+          qs: { token: accessToken, sync_token: '*', resource_types: '["user", "projects"]' },
+        },
+        function(error, response, body) {
+          const todoistSyncData = JSON.parse(body);
+          const todoistUser = todoistSyncData.user;
+          const todoistProjects = todoistSyncData.projects;
 
-      User.findOne({todoist_id: todoistUser.id}).then((currentUser) => {
-        if (currentUser) {
-          currentUser.set({
-            todoist_id: todoistUser.id,
-            email: todoistUser.email,
-            full_name: todoistUser.full_name,
-            avatar_big: todoistUser.avatar_big,
-            avatar_medium: todoistUser.avatar_medium,
-            avatar_s640: todoistUser.avatar_s640,
-            tz_info: {
-              timezone: todoistUser.tz_info.timezone,
-              gmt_string: todoistUser.tz_info.gmt_string
-            },
-            projects: todoistProjects,
-            karma: todoistUser.karma,
-            access_token: accessToken
-          })
-          currentUser.save()
-          done(error, currentUser)
-        } else {
-          new User({
-            todoist_id: todoistUser.id,
-            email: todoistUser.email,
-            full_name: todoistUser.full_name,
-            avatar_big: todoistUser.avatar_big,
-            avatar_medium: todoistUser.avatar_medium,
-            avatar_small: todoistUser.avatar_small,
-            avatar_s640: todoistUser.avatar_s640,
-            tz_info: {
-              timezone: todoistUser.tz_info.timezone,
-              gmt_string: todoistUser.tz_info.gmt_string
-            },
-            projects: todoistProjects,
-            karma: todoistUser.karma,
-            access_token: accessToken
-          }).save().then((newUser) => {
-            done(error, newUser)
-          })
+          User.findOne({ todoist_id: todoistUser.id }).then(currentUser => {
+            if (currentUser) {
+              currentUser.set({
+                access_token: accessToken,
+                avatar_big: todoistUser.avatar_big,
+                avatar_medium: todoistUser.avatar_medium,
+                email: todoistUser.email,
+                full_name: todoistUser.full_name,
+                projects: todoistProjects,
+                todoist_id: todoistUser.id,
+              });
+              currentUser.save();
+              done(error, currentUser);
+            } else {
+              new User({
+                access_token: accessToken,
+                avatar_big: todoistUser.avatar_big,
+                avatar_medium: todoistUser.avatar_medium,
+                email: todoistUser.email,
+                full_name: todoistUser.full_name,
+                projects: todoistProjects,
+                todoist_id: todoistUser.id,
+              })
+                .save()
+                .then(newUser => {
+                  done(error, newUser);
+                });
+            }
+          });
         }
-      })
+      );
     }
   )
-}))
+);
--- models/user-model.js
+++ models/user-model.js
@@ -1,28 +1,22 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  todoist_id: Number,
-  email: String,
-  full_name: String,
-  karma: Number,
   access_token: String,
-  avatar_s640: String,
   avatar_big: String,
   avatar_medium: String,
+  email: String,
+  full_name: String,
   projects: [
     {
       id: Number,
       name: String,
-      color: Number
-    }
+      color: Number,
+    },
   ],
-  tz_info: {
-    timezone: String,
-    gmt_string: String
-  }
-})
+  todoist_id: Number,
+});
 
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model('user', userSchema);
 
-module.exports = User
+module.exports = User;
